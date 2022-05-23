7b6a44853fbc1d3668189f9ed8c968af817487b1
Hyeonseong Lee <hslee7231@gmail.com>
2022-05-23 22:04:26 +0900

    heroku 배포(2)

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,9 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+const path = require('path');
 const target = 'http://127.0.0.1:3000';
 
 module.exports = {  // client 8080에서 서버를 호출하기 위한 path에 api라는 주소가 들어가면 target으로 mapping을 시킴
+  outputDir: path.resolve(__dirname, "../server/dist"),
   devServer: {
     port: 8080,
     proxy: {      // Server를 request하는 path의 주소 api가 있을 때 target으로 proxy함
